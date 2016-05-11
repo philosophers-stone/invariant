d5edaf72f4a9b4c08891d77affabc080ff8bc3eb
Booker Bense <bbense@uber.com>
2016-05-10 18:08:57 -0700

    wip

--- test/phst_invariant_test.exs
+++ test/phst_invariant_test.exs
@@ -7,7 +7,7 @@ defmodule Sample do
     if Enum.any?(arg1, fn(x) -> rem(x, 2) == 1 end) do
       Enum.zip(arg1, arg2)
     else
-      Enum.with_index(arg2)
+      Enum.with_index(arg1)
     end
   end
 
@@ -19,8 +19,20 @@ defmodule PhStInvariantTest do
   doctest PhStInvariant
 
   test "Sample test" do
-    assert Sample.test([2,4,6,8],[3,5]) == [{3, 0}, {5, 1}]
+    assert Sample.test([2,4,6,8],[3,5]) == [{2, 0}, {4, 1}, {6, 2}, {8, 3}]
+    assert Sample.test([2,5,6,8],[3,5]) == [{2, 3}, {5, 5}]
   end
 
+  test "congruent" do
+    {pass, fail} = PhStInvariant.congruent(Sample, :test, [[2,4,5,8],[3,5]], [{2, 3}, {4, 5}], 10)
+    refute fail == []
+  end
+
+  test "similar" do
+    {pass, fail} = PhStInvariant.similar(Sample, :test, [[2,4,6,8],[3,5]], [{3, 0}, {5, 1}], 10)
+
+    assert fail == []
+
+  end
 
 end
